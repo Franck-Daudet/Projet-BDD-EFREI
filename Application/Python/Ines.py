bec9a63c4a25e5f3a26f7a5a0c9b4a077fa22e40
Inès <ines.ameziane@efrei.net>
2023-03-14 13:51:03 +0100

    CRÉATION iNES.PY

---
+++ Application/Python/Ines.py
@@ -0,0 +1,15 @@
+import mysql.connector
+import plotly.express as px
+
+mydb=mysql.connector.connect(
+    host="143.42.63.50",
+    user="ines",
+    passwd="mbap",
+    database="Main"
+    )
+
+myc = mydb.cursor()
+myc.execute("select * from Credit")
+myres = myc.fetchall()
+for x in myres:
+    print(x)
