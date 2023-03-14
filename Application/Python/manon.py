c46b4a492ac68b4f7e537285063846e921dca116
manon brient <manon.brient@efrei.net>
2023-03-14 16:35:09 +0100

    ajout de requete

---
+++ Application/Python/manon.py
@@ -0,0 +1,5 @@
+
+request1= "SELECT nom,téléphone FROM `Etablissement` WHERE téléphone LIKE '07%';"
+request2= "SELECT nom, commune FROM `Etablissement`;"
+request3= "SELECT département FROM `Etablissement`;"
+request4= "SELECT nom,commune,academie From `Etablissement`WHERE academie ='Créteil';"
